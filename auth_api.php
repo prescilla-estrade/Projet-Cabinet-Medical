7473703c5e0f943f2903590dab001e03c22d146c
Mely2612 <iyamu34@gmail.com>
2024-03-07 10:53:08 +0100

    auth api (en cours)

--- auth_api.php
+++ auth_api.php
@@ -1,5 +1,25 @@
 <?php
-require('bd_connection.php');
+require('connectionBD_Auth.php');
 require('jwt_utils.php');
 
+$http_method = $_SERVER['REQUEST_METHOD'];
+if($http_method=="POST") {
 
+    $login=$_POST['login'];
+    $password=$_POST['password'];
+
+    $res = $linkpdo->prepare('SELECT login, password FROM authentification WHERE login = :login AND password = :password');
+    $res->execute(array('login' => $login, 'password' => $password));
+    $resultat = $res->fetchAll();
+
+    if (count($resultat) > 0) {
+        $token = generate_jwt($header, $payload, $secret);
+       
+    } else {
+        echo "Login ou mot de passe incorrect";
+    }
+}
+else {
+    echo "Méthode non autorisée";
+}
+?>
