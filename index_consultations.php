5d26c722875fe7200e02186d289901205a12f2e7
prescilla-estrade <prescilla.estrade@etu.iut-tlse3.fr>
2024-03-14 10:08:20 +0100

    index consultations et medecins finis

--- index_consultations.php
+++ index_consultations.php
@@ -2,4 +2,69 @@
 
 require('fonction_consultations.php');
 
+$http_method = $_SERVER['REQUEST_METHOD'];
+switch ($http_method){
+    case "GET" :
+        if(isset($_GET['id'])) {
+            $id = htmlspecialchars($_GET['id']);
+            $matchingData = get_consultations_id($id);
+            if ($matchingData) {
+                deliver_response(200, "Success", $matchingData);
+            } else {
+                deliver_response(404, "Not Found");
+            }
+        } else {
+            $matchingData = get_consultations();
+            deliver_response(200, "Success", $matchingData);
+        }
+        break;
+
+    case "POST" :
+        $data = json_decode(file_get_contents('php://input'), true);
+        $matchingData = create_consultations($data);
+        deliver_response(201, "Created", $matchingData);
+        break;
+        
+    case "PUT" :
+        $data = json_decode(file_get_contents('php://input'), true);
+        if(isset($_GET['id'])) {
+            $id = htmlspecialchars($_GET['id']);
+            $matchingData = update_consultations($id, $data);
+            if ($matchingData) {
+                deliver_response(200, "Success", $matchingData);
+            } else {
+                deliver_response(404, "Not Found");
+            }
+        } 
+        break;
+    
+    case "PATCH" :
+        $data = json_decode(file_get_contents('php://input'), true);
+        if(isset($_GET['id'])) {
+            $id = htmlspecialchars($_GET['id']);
+            $matchingData = update_consultations_partially($id, $data);
+            if ($matchingData) {
+                deliver_response(200, "Success", $matchingData);
+            } else {
+                deliver_response(404, "Not Found");
+            }
+        }
+        break;
+           
+    case "DELETE" :
+        if(isset($_GET['id'])) {
+            $id = htmlspecialchars($_GET['id']);
+            $matchingData = delete_consultations($id);
+            if ($matchingData) {
+                deliver_response(200, "Success", $matchingData);
+            } else {
+                deliver_response(404, "Not Found");
+            }
+        }
+        break;
+        
+    default:
+        deliver_response(400, "Bad Request");
+        break;
+}
 ?>
--- index_medecins.php
+++ index_medecins.php
@@ -2,4 +2,69 @@
 
 require('fonction_medecins.php');
 
+$http_method = $_SERVER['REQUEST_METHOD'];
+switch ($http_method){
+    case "GET" :
+        if(isset($_GET['id'])) {
+            $id = htmlspecialchars($_GET['id']);
+            $matchingData = get_medecins_id($id);
+            if ($matchingData) {
+                deliver_response(200, "Success", $matchingData);
+            } else {
+                deliver_response(404, "Not Found");
+            }
+        } else {
+            $matchingData = get_medecins();
+            deliver_response(200, "Success", $matchingData);
+        }
+        break;
+
+    case "POST" :
+        $data = json_decode(file_get_contents('php://input'), true);
+        $matchingData = create_medecins($data);
+        deliver_response(201, "Created", $matchingData);
+        break;
+        
+    case "PUT" :
+        $data = json_decode(file_get_contents('php://input'), true);
+        if(isset($_GET['id'])) {
+            $id = htmlspecialchars($_GET['id']);
+            $matchingData = update_medecins($id, $data);
+            if ($matchingData) {
+                deliver_response(200, "Success", $matchingData);
+            } else {
+                deliver_response(404, "Not Found");
+            }
+        } 
+        break;
+    
+    case "PATCH" :
+        $data = json_decode(file_get_contents('php://input'), true);
+        if(isset($_GET['id'])) {
+            $id = htmlspecialchars($_GET['id']);
+            $matchingData = update_medecins_partially($id, $data);
+            if ($matchingData) {
+                deliver_response(200, "Success", $matchingData);
+            } else {
+                deliver_response(404, "Not Found");
+            }
+        }
+        break;
+           
+    case "DELETE" :
+        if(isset($_GET['id'])) {
+            $id = htmlspecialchars($_GET['id']);
+            $matchingData = delete_medecins($id);
+            if ($matchingData) {
+                deliver_response(200, "Success", $matchingData);
+            } else {
+                deliver_response(404, "Not Found");
+            }
+        }
+        break;
+        
+    default:
+        deliver_response(400, "Bad Request");
+        break;
+}
 ?>
