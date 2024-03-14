6095db0f1a48b978bdf16ce369ef8dc55d0e1ef7
Mely2612 <iyamu34@gmail.com>
2024-03-14 10:47:34 +0100

    modif index stat

--- index_stat.php
+++ index_stat.php
@@ -6,13 +6,8 @@ $http_method = $_SERVER['REQUEST_METHOD'];
 if ($http_method === "GET") {
     $matchingData = get_repartition_usagers(); 
     $matchingData2 = get_duree_consultation(); 
-    if ($matchingData) {
+    if ($matchingData AND $matchingData2) {
         deliver_response(200, "Success", $matchingData);
-    } else {
-        deliver_response(405, "Method Not Allowed");
-    }
-
-    if ($matchingData2) {
         deliver_response(200, "Success", $matchingData2);
     } else {
         deliver_response(405, "Method Not Allowed");
